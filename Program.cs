65971aff7f63b77fe27ae1c0c5245da986b6b412
alfredoabella <alfredoabelladorronsoro@gmail.com>
2018-10-18 10:12:23 +0200

    Inserción inicial en el repositorio del módulo EleneTroya

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EleneTroya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
